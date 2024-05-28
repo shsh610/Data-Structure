c8749b0a51092908b14df41fb53f2731304c0767
시헌 송 <shsh610@naver.com>
2024-05-28 16:51:43 +0900

    초기 커밋.

---
+++ Data Structure.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// LIFO : Last In First Out - STACK
+// FIFO : First In First Out - QUEUE
+
+int main()
+{
+	int last{ 0 };
+	int type{ 0 };
+	int value[999];
+
+	while (true)
+	{
+		std::cout << "1. push, 2: pop >";
+		std::cin >> type;
+		if (type == 1)
+		{
+			std::cout << "value?";
+			std::cin >> value[last];
+			last++;
+			std::cout << "--STACK--" << std::endl;
+			for (int i = 0; i < last; i++)
+			{
+
+			}
+		}
+	}
+}
