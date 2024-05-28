42debd633f240f89197a002ad2da0355bd30a635
시헌 송 <shsh610@naver.com>
2024-05-28 17:55:40 +0900

    포인터

--- Data Structure.cpp
+++ Data Structure.cpp
@@ -3,26 +3,43 @@
 // LIFO : Last In First Out - STACK
 // FIFO : First In First Out - QUEUE
 
+//int main()
+//{
+//	int last{ 0 };
+//	int type{ 0 };
+//	int value[999];
+//
+//	while (true)
+//	{
+//		std::cout << "1. push, 2: pop >";
+//		std::cin >> type;
+//		if (type == 1)
+//		{
+//			std::cout << "value?";
+//			std::cin >> value[last];
+//			last++;
+//			std::cout << "--STACK--" << std::endl;
+//			for (int i = 0; i < last; i++)
+//			{
+//
+//			}
+//		}
+//	}
+//}
+
+struct Bullet
+{
+	int damage;
+	float speed;
+	float size;
+
+	Bullet *pNext;
+};
+
 int main()
 {
-	int last{ 0 };
-	int type{ 0 };
-	int value[999];
+	Bullet *a = new Bullet;
+	Bullet *b = new Bullet;
 
-	while (true)
-	{
-		std::cout << "1. push, 2: pop >";
-		std::cin >> type;
-		if (type == 1)
-		{
-			std::cout << "value?";
-			std::cin >> value[last];
-			last++;
-			std::cout << "--STACK--" << std::endl;
-			for (int i = 0; i < last; i++)
-			{
-
-			}
-		}
-	}
+	a->pNext = b;
 }
